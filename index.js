f7fbf9852854434839b783588913a4e3d0cae8c4
ranahusain <capthussain2point0@gmail.com>
2025-07-11 10:41:41 +0500

    Authentication With Github Done

--- index.js
+++ index.js
@@ -20,7 +20,53 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 passport.use(
-  new GithubStrategy({
-    clientID: "ac51e75339d5e560e5defdade2c0f59bdad19aef",
-  })
+  new GithubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/github/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+  )
 );
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get("/", (req, res) => {
+  res.send('<a href="auth/github/callback">Login With GitHub </a>');
+});
+
+app.get(
+  "/auth/github",
+  passport.authenticate("github", { scope: ["user:email"] })
+);
+
+app.get(
+  "/auth/github/callback",
+  passport.authenticate("github", { failureRedirect: "/" }),
+  (req, res) => {
+    res.redirect("/profile");
+  }
+);
+
+app.get("/profile", (req, res) => {
+  res.send(`Hello ${req.user.username}`);
+});
+
+app.get("/logout", (req, res) => {
+  req.logout(() => {
+    res.redirect("/");
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running at PORT 3000");
+});
